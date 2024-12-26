45a1d2284048426f21ab3a108e0997b2dac49efa
charan <21pa1a6137@vishnu.edu.in>
2024-12-26 19:59:43 +0530

    main.py line 19 20 commented

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
 st.set_page_config(page_title="LearnFlex: Personalized Learning Assistant", layout="wide")
 
